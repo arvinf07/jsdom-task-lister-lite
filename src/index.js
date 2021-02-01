d0808cf79ff43131b79165cd66998c6341f6ba7b
Arvin Fernandez <arvinf07@gmail.com>
2021-02-01 10:59:51 -0500

    MVP + delete ability

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let field = document.getElementById('new-task-description');
+  let taskList = document.getElementById('tasks');
+
+  document.querySelector('form').addEventListener("submit", function(event){
+    event.preventDefault()
+    taskList.innerHTML += `<li>${field.value}<button>X</button></li>`
+    
+    let buttons = document.querySelectorAll('li button')
+    for (let i = 0; i < buttons.length; i++){
+      buttons[i].addEventListener('click', (event) => {
+        buttons[i].parentNode.remove()
+      })
+    }
+    document.querySelector("form").reset()
+  });
 });
